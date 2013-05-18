6797dc9535848472681fe537fc8bd6f9e6626f81
Max Jacobson <maxwell.jacobson@gmail.com>
2013-05-18 17:17:31 -0400

    httparty rules

--- web.rb
+++ web.rb
@@ -1,14 +1,9 @@
 require 'sinatra'
 require 'haml'
-require 'net/http'
 require 'uri'
 require 'json'
-require 'pp'
+require 'httparty'
 require_relative 'helpers.rb' # helper methods
-
-# enable :sessions
-# set :dump_errors, false
-# set :show_exceptions, false
 
 set :protection, :except => :frame_options
 
@@ -28,11 +23,7 @@ end
 get '/widget/search/:query' do
   @query = params[:query]
   url = URI.escape "https://api.readmill.com/v2/books/search?query=#{@query}&client_id=cebbc653b49578b9d02a7c23a89dc5e3"
-  uri = URI.parse url
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  json = http.get(uri.request_uri).body
-  hash = JSON.parse(json)
+  hash = HTTParty.get url
   @books = hash["items"]
   puts @books
   haml :search
